0c61ef08a9cfb5a82d4f661a5afb840a0d3dad62
aloha-fim <frederec.im@gmail.com>
2023-11-30 15:32:57 +0800

    react learn simple

--- frontend-react/src/components/BetBox.tsx
+++ frontend-react/src/components/BetBox.tsx
@@ -1,11 +1,31 @@
+import BetButton from "./BetButton";
 
+type Props = {
+    betDirection: string;
+    setBetDirection: any;
+    isLoading: boolean;
+    hasWon: boolean;
+    valStored: number;
+};
 
-function BetBox() {
+function BetBox({ betDirection, setBetDirection, isLoading, hasWon, valStored }: Props) {
   return (
     <div className="mt-5">
-        <div className="py-2 border bg-red-100">Layer1</div>
-        <div className="py-2 border bg-blue-100">Layer2</div>
-        <div className="py-2 border bg-fuchsia-100">Layer3</div>
+        <div className="py-4 border bg-gray-800 text-white text-right pr-5">
+            {isLoading ? "Loading..." : `Random Number Returned: ${valStored}`}
+        </div>
+        <div className="flex flex-row justify-between py-2 border">
+            <BetButton direction="down" betDirection={betDirection} setBetDirection={setBetDirection} />
+            <BetButton direction="up" betDirection={betDirection} setBetDirection={setBetDirection} />
+            <div></div>
+        </div>
+        <div className="py-2 border bg-gray-200 text-center text-4xl font-bold">
+            {valStored != 0 && (
+                <div className={"" + (hasWon ? "text-green-500" : "text-red-500")}>
+                    {hasWon ? "WINNER" : "BAD LUCK"}
+                </div>
+            )}
+        </div>
     </div>
   );
 }
--- frontend-react/src/components/Button.tsx
+++ frontend-react/src/components/Button.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 function Button({ runFunction }: Props) {
   return (
-    <button onClick={runFunction} className="transition-all duration-300 border border-black w-full mt-4 py-12 rounded-sm bg-indigo-500 hover:bg-indigo-600 text-white">
+    <button onClick={runFunction} className="transition-all duration-300 border border-gray-200 w-full mt-4 py-12 rounded-xl bg-indigo-500 hover:bg-indigo-600 text-white">
         Sample Button
     </button>
 
