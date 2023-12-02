c5f19b451f9d9126fe20bd559c4c05afa1e59259
aloha-fim <frederec.im@gmail.com>
2023-12-03 01:09:14 +0800

    recorder icon

--- frontend-react/src/components/Controller.tsx
+++ frontend-react/src/components/Controller.tsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 import Title from "./Title";
+import axios from "axios";
+import RecordMessage from "./RecordMessage";
 
 function Controller() {
   const [isLoading, setIsLoading] = useState(false);
   const [messages, setMessages] = useState<any[]>([]);
 
-  const createBlobUrl = (data: any) => {};
+  function createBlobUrl(data: any) {
+    const blob = new Blob([data], { type: "audio/mpeg" });
+    const url = window.URL.createObjectURL(blob);
+    return url;
+  };
 
-  const handleStop = async () => {};
+  const handleStop = async (blobUrl: string) => {
+    setIsLoading(true);
+  };
 
   return (
     <div className="h-screen overflow-y-hidden">
@@ -16,7 +24,7 @@ function Controller() {
         {/* Recorder */}
         <div className="fixed bottom-0 w-full py-6 border-t text-center bg-gradient-to-r from-sky-500 to-green-500">
           <div className="flex justify-center items-center w-full">
-            <div>Recorder</div>
+            <RecordMessage handleStop={handleStop} />
           </div>
         </div>
       </div>
